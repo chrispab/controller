f99c4e9e73bfe26a43095f653fc280461c7a7463
chrispab <cbattisson@gmail.com>
2017-11-08 19:32:15 +0000

    v99 - throttle email clear flag

--- control.py
+++ control.py
@@ -152,6 +152,7 @@ def main():
         startT = time.time()
         
         #read sensor
+        sensorMessage = ''
         humidity, temperature,sensorMessage = ctl1.sensor1.read()
         if sensorMessage :
             #emailMe.sendemail(zone + ': bad sensor reads ' + str(maxSensorReadErrors) + '  - PowerCycle', self.message)	
