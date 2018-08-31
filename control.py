04cc7ed415d7cc1d15a4a5c6c4e18fdf373e9838
chrispab <cbattisson@gmail.com>
2018-08-31 14:32:46 +0100

    websocket 2

--- control.py
+++ control.py
@@ -36,7 +36,7 @@ import tornado.httpserver
 import tornado.websocket
 import tornado.ioloop
 import tornado.web
-# import socket
+import socket
 
 Broker = "192.168.0.200"
 
@@ -160,6 +160,32 @@ client.loop_start()
 #    time.sleep(1*60)
 ##################################################    
 
+'''
+This is a simple Websocket Echo server that uses the Tornado websocket handler.
+Please run `pip install tornado` with python of version 2.7.9 or greater to install tornado.
+This program will echo back the reverse of whatever it recieves.
+Messages are output to the terminal for debuggin purposes. 
+''' 
+ 
+class WSHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print 'new connection'
+      
+    def on_message(self, message):
+        print 'message received:  %s' % message
+        # Reverse Message and send it back
+        print 'sending back message: %s' % message[::-1]
+        self.write_message(message[::-1])
+ 
+    def on_close(self):
+        print 'connection closed'
+ 
+    def check_origin(self, origin):
+        return True
+ 
+application = tornado.web.Application([
+    (r'/ws', WSHandler),
+])
 #cfg.
 def main():
 
