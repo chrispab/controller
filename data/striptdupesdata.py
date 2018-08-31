99f653c4bc7209fbbad0bcdb4f4ad63685b98f5a
chrispab <cbattisson@gmail.com>
2018-08-31 11:11:16 +0100

    move old files

--- data/striptdupesdata.py
+++ data/striptdupesdata.py
@@ -1,5 +1,5 @@
-#To be able to read csv formated files, we will first have to import the
-#csv module.
+# To be able to read csv formated files, we will first have to import the
+# csv module.
 import csv
 
 csvfilename = '/home/pi/projects/controller1/thdata.csv'
@@ -8,47 +8,47 @@ oppath = "/home/pi/projects/controller1/thdatacleaned.csv"
 
 import csv
 
+
 def csv_reader(file_obj):
 
     reader = csv.reader(file_obj)
     csv_file = open(oppath, "wb")
     writer = csv.writer(csv_file, delimiter=',')
-    
+
     dupe = False
-    row = next(reader) #get 1st row
+    row = next(reader)  # get 1st row
     while row:
-        print('reading row :',row) 
-        #for row in reader:
-        #if dupe == True:
+        print('reading row :', row)
+        # for row in reader:
+        # if dupe == True:
         try:
             nextRow = next(reader)
-            print('next    row :',nextRow)
+            print('next    row :', nextRow)
             dupe = False
-            #while ( (row[1] == nextRow[1]) and (row[3] == nextRow[3]) and (row[4] == nextRow[4]) and (row[5] == nextRow[5]) ):
-            if ( (row[1] == nextRow[1]) and (row[3] == nextRow[3]) and (row[4] == nextRow[4]) and (row[5] == nextRow[5]) ):    
-                print('dupe1 row  : ',row)
-                print('dupe2 row  : ',nextRow)
+            # while ( (row[1] == nextRow[1]) and (row[3] == nextRow[3]) and (row[4] == nextRow[4]) and (row[5] == nextRow[5]) ):
+            if ((row[1] == nextRow[1]) and (row[3] == nextRow[3]) and (row[4] == nextRow[4]) and (row[5] == nextRow[5])):
+                print('dupe1 row  : ', row)
+                print('dupe2 row  : ', nextRow)
                 dupe = True
-            
+
             #nextRow = next(reader)
             if dupe == True:
                 row = nextRow
-            
+
             if dupe == False:
-                print('writing row :',row)            
+                print('writing row :', row)
                 print ('-')
                 writer.writerow(row)
                 row = nextRow
-            
+
         except:
             csv_file.close()
             print(oppath, "written to disk after iter exception")
-            break 
-        #finally:
+            break
+        # finally:
             #print(oppath, "written to disk")
-            
+
     print(oppath, "written to disk")
-            
 
 
 def csv_writer(data, path):
@@ -62,26 +62,22 @@ def csv_writer(data, path):
             writer.writerow(line)
 
 
-#----------------------------------------------------------------------
+# ----------------------------------------------------------------------
 
 if __name__ == "__main__":
-
 
     with open(csv_path, "rb") as f_obj:
 
         csv_reader(f_obj)
-        
-        
-        
-        #with open(csvfilename, 'rb') as f:
+
+        # with open(csvfilename, 'rb') as f:
     #reader = csv.reader(f)
-    #for row in reader:
+    # for row in reader:
         #print row
-        
-        
-        #with open('/home/pi/projects/controller1/thdata.csv', 'r') as f:
+
+        # with open('/home/pi/projects/controller1/thdata.csv', 'r') as f:
         #data = list(reader(f))
-    
+
     ##listlen = len(data)
     #startsample = 1
 
