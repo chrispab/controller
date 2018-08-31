21def8b7e603f1b6c62914043772ba026cec624a
chrispab <cbattisson@gmail.com>
2018-09-01 00:29:27 +0100

    tweak

--- control.py
+++ control.py
@@ -32,11 +32,11 @@ import sendemail as emailMe
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 
-import tornado.httpserver
-import tornado.websocket
-import tornado.ioloop
-import tornado.web
-import socket
+# import tornado.httpserver
+# import tornado.websocket
+# import tornado.ioloop
+# import tornado.web
+# import socket
 
 Broker = "192.168.0.200"
 
@@ -167,25 +167,25 @@ This program will echo back the reverse of whatever it recieves.
 Messages are output to the terminal for debuggin purposes. 
 ''' 
  
-class WSHandler(tornado.websocket.WebSocketHandler):
-    def open(self):
-        print 'new connection'
+# class WSHandler(tornado.websocket.WebSocketHandler):
+#     def open(self):
+#         print 'new connection'
       
-    def on_message(self, message):
-        print 'message received:  %s' % message
-        # Reverse Message and send it back
-        print 'sending back message: %s' % message[::-1]
-        self.write_message(message[::-1])
+#     def on_message(self, message):
+#         print 'message received:  %s' % message
+#         # Reverse Message and send it back
+#         print 'sending back message: %s' % message[::-1]
+#         self.write_message(message[::-1])
  
-    def on_close(self):
-        print 'connection closed'
+#     def on_close(self):
+#         print 'connection closed'
  
-    def check_origin(self, origin):
-        return True
+#     def check_origin(self, origin):
+#         return True
  
-application = tornado.web.Application([
-    (r'/ws', WSHandler),
-])
+# application = tornado.web.Application([
+#     (r'/ws', WSHandler),
+# ])
 #cfg.
 def main():
 
@@ -216,6 +216,7 @@ def main():
     emailObj.send( "Zone " + zoneNumber + " " + emailzone + location + ' - Process Started', message)
                 
     while 1:
+        # tornado.ioloop.IOLoop.instance().loop()
 
         logger.info("=main while loop=")
         #logger.warning("== process uptime: %s =",processUptime)
@@ -349,5 +350,11 @@ def main():
 
 
 if __name__ == "__main__":
-    # stuff only to run when not called via 'import' here
+    # # stuff only to run when not called via 'import' here
+    # http_server = tornado.httpserver.HTTPServer(application)
+    # http_server.listen(8888)
+    # # tornado.ioloop.IOLoop.instance().start()
+    # tornado.ioloop.IOLoop.instance().run_sync(main)
+
+
     main()
